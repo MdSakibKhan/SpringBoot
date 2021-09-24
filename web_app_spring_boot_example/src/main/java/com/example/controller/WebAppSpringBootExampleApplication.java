b0be9e44bbd564c6c18a785527ba0eb6a10975de
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-09-24 18:29:49 +0600

    Web example with spring boot jsp

---
+++ web_app_spring_boot_example/src/main/java/com/example/controller/WebAppSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringBootExampleApplication.class, args);
+	}
+
+}
